0b697ee92dba85c3de5a720e104354ca248452dc
Ravi Makawana <rvmakawana@gmail.com>
2024-02-25 12:03:38 -0600

    Account microservice DTO,Controller,entity,repository

---
+++ src/main/java/com/bank/accounts/controller/AccountsController.java
@@ -0,0 +1,24 @@
+package com.bank.accounts.controller;
+
+import com.bank.accounts.constants.AccountConstants;
+import com.bank.accounts.dto.CustomerDto;
+import com.bank.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api",produces = {MediaType.APPLICATION_JSON_VALUE})
+public class AccountsController {
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+
+
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountConstants.STATUS_201,AccountConstants.MESSAGE_201));
+    }
+}
