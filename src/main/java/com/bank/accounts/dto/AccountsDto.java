41756254379b273131cdcb5bb8a7c76b684cfe2e
Ravi Makawana <rvmakawana@gmail.com>
2024-03-01 19:27:02 -0600

    Add Validations

--- src/main/java/com/bank/accounts/dto/AccountsDto.java
+++ src/main/java/com/bank/accounts/dto/AccountsDto.java
@@ -1,11 +1,18 @@
 package com.bank.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
-
+    @Pattern(regexp = "^$[0-9]{10}", message = "Invalid mobile number")
+    @NotEmpty(message = "Account number must not be empty or null")
     private Long accountNumber;
+
+    @NotEmpty(message = "Account type must not be empty or null")
     private String accountType;
+
+    @NotEmpty(message = "Branch address must not be empty or null")
     private String branchAddress;
 }
--- src/main/java/com/bank/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bank/accounts/exception/GlobalExceptionHandler.java
@@ -21,7 +21,20 @@ import java.util.Map;
 
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler  extends ResponseEntityExceptionHandler {
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request){
+        Map<String ,String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+        validationErrorList.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+                                                                  )
 
    @ExceptionHandler(CustomerAlreadyExistsException.class)
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
