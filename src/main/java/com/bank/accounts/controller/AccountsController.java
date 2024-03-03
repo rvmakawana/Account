8b4bcb8e934305ecfb79ce92ab78a1d22c0b695a
Ravi Makawana <rvmakawana@gmail.com>
2024-03-02 19:39:25 -0600

    Add @schema Annotation

--- src/main/java/com/bank/accounts/controller/AccountsController.java
+++ src/main/java/com/bank/accounts/controller/AccountsController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
